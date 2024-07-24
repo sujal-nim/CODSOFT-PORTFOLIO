1601a8c0750edccf808055b47e662d112631b01d
Sujal Nimbalkar <164552864+sujal-nim@users.noreply.github.com>
2024-07-24 22:33:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//import Typed from 'typed.js'
+var typed = new Typed("#text", {
+    Strings :["Frontend Developer" , "Web Developer" , "Creator"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})   
